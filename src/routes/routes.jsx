687ebadbe67705027c3374e8c6d2ea346d05b049
Mahim <mahimmasrafi04@gmail.com>
2024-09-01 04:48:31 +0600

    fixed deployment

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,7 +6,6 @@ import Page1 from "../pages/Page1";
 import Page2 from "../pages/Page2";
 import Page3 from "../pages/Page3";
 import Page4 from "../pages/Page4";
-import Page5 from "../pages/Page5";
 
 const router = createHashRouter([
   {
